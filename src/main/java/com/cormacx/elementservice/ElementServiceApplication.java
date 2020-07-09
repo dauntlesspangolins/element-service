68281b1377d605243e80bcb6d32a3734d7d95b93
Lucas Gaio <gaio.lucas0@gmail.com>
2020-07-09 12:57:31 -0300

    Renamed package name to avoid duplication

--- src/main/java/com/cormacx/elementservice/ElementServiceApplication.java
+++ src/main/java/com/cormacx/elementservice/ElementServiceApplication.java
@@ -1,4 +1,4 @@
-package com.cormacx.elementservice.elementservice;
+package com.cormacx.elementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/cormacx/elementservice/ElementServiceApplicationTests.java
+++ src/test/java/com/cormacx/elementservice/ElementServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cormacx.elementservice.elementservice;
+package com.cormacx.elementservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
