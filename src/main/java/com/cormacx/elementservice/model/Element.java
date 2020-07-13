7a5e2bcfdf9cb1abdf90a44dffff5f9064cd2335
Lucas Gaio <gaio.lucas0@gmail.com>
2020-07-13 17:06:05 -0300

    Added attributes to Element;

--- src/main/java/com/cormacx/elementservice/model/Element.java
+++ src/main/java/com/cormacx/elementservice/model/Element.java
@@ -1,5 +1,37 @@
 package com.cormacx.elementservice.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
 public class Element {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true)
+	private String name;
+	
+	@Column(unique = true)
+	private Integer atomicNumber;
+	
+	@Column(unique = true)
+	private String symbol;
+	
+	private String group;
+	
+	private String period;
+	
+	private Float atomicWeight;
+	
+	private Float densityGramsPerCubicCentimeter;
+	
+	
 }
--- src/test/java/com/cormacx/elementservice/repository/ElementRepositoryTest.java
+++ src/test/java/com/cormacx/elementservice/repository/ElementRepositoryTest.java
@@ -1,12 +1,22 @@
 package com.cormacx.elementservice.repository;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.cormacx.elementservice.model.Element;
 
 @SpringBootTest
 public class ElementRepositoryTest {
 
 	@Autowired
 	ElementRepository elementRepo;
+	
+	Element hydrogen = new Element();
+	
+	@Test
+	public void save_throwsExceptionIfRepeated() {
+		
+	}
 	
 }
